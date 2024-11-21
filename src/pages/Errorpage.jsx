357a0fc80e247eb345b1aed231e6fc7cf1c33894
rasel mridha <raselmridha792@gmail.com>
2024-11-21 11:22:56 +0600

    add error page, handling some issue, add environment variable, and set firebase deployment

--- src/pages/Errorpage.jsx
+++ src/pages/Errorpage.jsx
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
 
 const Errorpage = () => {
-    return (
-        <>
-        <h2 className="text-2xl font-bold">Not found</h2>
-        <Link to="/" className="btn">Go to home</Link>      
-        </>
-    );
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="text-center p-8 rounded-lg shadow-lg bg-white">
+          <h1 className="text-6xl font-bold text-red-600">Oops!</h1>
+          <p className="text-xl mt-4 text-gray-700">
+            Something went wrong. Page Not found.
+          </p>
+          <div className="mt-6">
+            <Link
+              to="/"
+              className="btn btn-neutral btn-lg mt-4" 
+            >
+              Go to Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Errorpage;
